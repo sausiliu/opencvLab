b4f2b03fdeb8668a0a27c6b16d4e3c8b0d5bc840
RichardLiu <liujh16@lenovo.com>
2018-08-24 16:03:19 +0800

    update project, the program run successful

--- face.py
+++ face.py
@@ -1,28 +1,29 @@
 # coding:utf-8
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-import cv2
+# import sys
+import cv2 as cv
 
-imagepath = r'./face.jpg'
-face_cascade = cv2.CascadeClassifier(r'./haarcascade_frontalcatface.xml')
-image = cv2.imread(imagepath)
+imagePath = r'./face.jpg'
+haarCascadePath = r'./data/haarcascade/haarcascade_frontalface_default.xml'
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+face_cascade = cv.CascadeClassifier(haarCascadePath)
+image = cv.imread(imagePath)
+
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(
     gray,
     scaleFactor=1.15,
     minNeighbors=5,
     minSize=(5, 5),
-    flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    flags=cv.CASCADE_SCALE_IMAGE
 )
+# flags=cv.CV_HAAR_SCALE_IMAGE
 
-print "发现{0}个人脸!".format(len(faces))
+print("发现{0}个人脸!".format(len(faces)))
 
 for (x, y, w, h) in faces:
-    # cv2.rectangle(image,(x,y),(x+w,y+w),(0,255,0),2)
-    cv2.circle(image, ((x + x + w) / 2, (y + y + h) / 2), w / 2, (0, 255, 0), 2)
+    cv.rectangle(image, (x, y), (x+w, y+w), (0, 255, 0), 2)
+    # cv.circle(image, ((x+x+w)/2, (y+y+h)/2), w/2, (0,255,0), 2)
 
-cv2.imshow("Find Faces!", image)
+cv.imshow("Find Faces!", image)
 
-cv2.waitKey(0)
+cv.waitKey(0)
