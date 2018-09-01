fb81518968e6e471734247be7b3f862f7ff48cfd
Bird <sausiliu@163.com>
2018-09-01 16:03:00 +0800

    update opencv_api.md, update rectangle_detector.py

--- doc/opencv_api.md
+++ doc/opencv_api.md
@@ -4,14 +4,14 @@
 
 ### 图像处理
 
-1. cvtColor
+* cvtColor
 获取二值图
 
 
-2. GaussianBlur
+. GaussianBlur
 
 
-3. Canny
+* Canny
 
 image 单通道输入图像<br>
 edges 单通道存储边缘的输出图像<br> 
@@ -19,7 +19,32 @@ threshold1 第一个阈值<br>
 threshold2 第二个阈值<br>
 aperture_size Sobel算子内核大小
 
-4. approxPolyDP
+```
+滞后阈值: 最后一步，Canny 使用了滞后阈值，滞后阈值需要两个阈值(高阈值和低阈值):
+
+如果某一像素位置的幅值超过 高 阈值, 该像素被保留为边缘像素。
+如果某一像素位置的幅值小于 低 阈值, 该像素被排除。
+如果某一像素位置的幅值在两个阈值之间,该像素仅仅在连接到一个高于高阈值的像素时被保留。
+Canny 推荐的 高:低 阈值比在 2:1 到3:1之间。
+```
+
+* approxPolyDP
+
+
+* findContours
+
+findContour用来寻找图像中的轮廓信息。
+
+
+* drawContours
+
+
+Demo:
+
+contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img,contours,-1,(0,0,255),3)
+
+
 
 
 ![avatar][1]
--- rubik/rectangle_detector.py
+++ rubik/rectangle_detector.py
@@ -5,7 +5,8 @@ import cv2 as cv
 import math
 #import numpy as np
 
-img_path = './data/images/demo1.png'
+#img_path = './data/images/demo1.png'
+img_path = './data/images/demo4.png'
 
 
 img_orig = cv.imread(img_path)
@@ -18,41 +19,47 @@ cv.namedWindow("gray image", cv.WINDOW_NORMAL)
 cv.imshow("gray image", image)
 
 # Gaussian Filter
-image = cv.GaussianBlur(image, (5, 5), 80)
-cv.namedWindow("Gaussian Blurred", cv.WINDOW_NORMAL)
-cv.imshow("Gaussian Blurred", image)
+# image = cv.GaussianBlur(image, (5, 5), 80)
+# cv.namedWindow("Gaussian Blurred", cv.WINDOW_NORMAL)
+# cv.imshow("Gaussian Blurred", image)
 
 # Canny Filter
 
-image = cv.Canny(image, 0, 25, 0)
+image = cv.Canny(image, 20, 40, 3)
 cv.namedWindow("Edge", cv.WINDOW_NORMAL)
 # 圆滑边缘信息
-image = cv.GaussianBlur(image, (3, 3), 2)
+# image = cv.GaussianBlur(image, (3, 3), 2)
 # image = cv.bilateralFilter(image, 9, 75, 75)
 
 cv.imshow("Edge", image)
 
+img = img_orig
 img2, contours, hierarchy = cv.findContours(image, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
+cv.drawContours(img, contours, -1, (0, 0, 255), 3)
+cv.namedWindow("Contours", cv.WINDOW_NORMAL)
+cv.imshow("Contours", img)
+
+print("Contours length:")
 print(len(contours))
 
-img = img_orig
 for cnt in contours:
     x, y, w, h = cv.boundingRect(cnt)
-    if w < 60 or h < 60:
-        continue
+    #if w < 60 or h < 60:
+    #    continue
     # print("x:{0}, y:{1} , w:{2}, h: {3}".format(x, y, w, h))
-    # cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    approx = cv.approxPolyDP(cnt, 2, True)
+    #cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    approx = cv.approxPolyDP(cnt, 50, True)
+    cv.polylines(img, [approx], True, (0, 0, 255), 2)
     # if approx.size == 4 \
     #         and math.fabs(cv.contourArea(cnt)) > 5 \
     #         and cv.isContourConvex(cnt):
-    # print(approx.size)
-    if approx.size > 20:
-        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    print(approx.size)
+    #if approx.size == 4:
+    #    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-cv.namedWindow("Output", cv.WINDOW_NORMAL)
-cv.imshow("Output", img)
+# cv.namedWindow("Output", cv.WINDOW_NORMAL)
+# cv.imshow("Output", img)
 
 cv.waitKey()
 cv.destroyAllWindows()
